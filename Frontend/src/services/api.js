48271ba2308f68292c512b3fbc3df1e48cd4c4d4
PRAVESH TIWARI <pravesht252@gmail.com>
2026-06-19 21:42:33 +0530

    SCAN

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "https://scan-bite-ten.vercel.app/";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export const fetchProducts = async () => {
   const res = await fetch(`${BASE_URL}/products`);
