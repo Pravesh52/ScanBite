a305720582a328ec5b9a30894bb801b684369499
PRAVESH TIWARI <pravesht252@gmail.com>
2026-06-20 09:49:57 +0530

    app.js

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,10 @@ const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://scanbite-xxxx.vercel.app", "http://localhost:3000"],
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
